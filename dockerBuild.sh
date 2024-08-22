cf7d8e08aefd99da8c295864f4cb24336ced56a0
Rangam-1 <135948672+Rangam-1@users.noreply.github.com>
2024-08-22 17:36:52 +0530

    Update dockerBuild.sh

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,4 +1,6 @@
 docker build -t tour:v1 .
 docker build -f dockerfile1 -t learning:v1 .
 docker build -f dockerfile2 -t youtube:v1 .
-
+docker tag youtube:v1 rangamvenky/electronics:latest
+docker tag learning:v1 rangamvenky/e-learning:latest
+docker tag tour:v1 rangamvenky/tour:latest
